2cf843bc77fa1c9bbea768488e9ee84965b05200
lucasger78 <lucasger78@hotmail.com>
2025-04-04 11:24:33 -0300

    Agregado Procfile y requirements.txt

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
-apt-get update && apt-get install -y portaudio19-dev
-pip install -r requirements.txt
+apt-get update && apt-get install -y \
+    portaudio19-dev \
+    libasound2-dev \
+    libjack-dev \
+    libpulse-dev \
+    libffi-dev  # Agrega dependencias adicionales necesarias para compilar PyAudio
+
+pip install --upgrade pip setuptools wheel  # Asegura que pip está actualizado
+pip install -r requirements.txt  # Luego instala las dependencias
